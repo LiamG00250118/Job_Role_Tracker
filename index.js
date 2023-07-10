06cde459efafe96d5a5b5a55beca74e3acdf2b73
Liamb44 <liamrbrandt@gmail.com>
2023-07-10 17:25:08 -0400

    added the functions, currently working with sql database

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const inquirer = require("inquirer");
+const mysql = require('mysql2');
 
-// : view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Airpr0963!',
+    database: 'manager_hub'
+  });
 
 function startPrompts() {
     console.log('Welcome to the Manager Hub');
@@ -45,29 +51,140 @@ function startPrompts() {
 }
 
 function viewDepartments() {
-    console.log('1');
+    connection.query('SELECT * FROM departments', (error, results) => {
+        if (error) throw error;
+      
+        console.log('\n Departments:');
+        console.log(results);
+      });
 
+    startPrompts();
 }
 function viewRoles() {
-    console.log('2');
+    connection.query('SELECT * FROM roles', (error, results) => {
+        if (error) throw error;
+      
+        console.log('\n Roles:');
+        console.log(results);
+      });
+
+    startPrompts();
 
 }
 function viewEmployees() {
-    console.log('3');
+    connection.query('SELECT * FROM employees', (error, results) => {
+        if (error) throw error;
+      
+        console.log('\n Employees:');
+        console.log(results);
+      });
+
+    startPrompts();
 
 }
 function addDepartment() {
-    console.log('4');
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'departmentID',
+        message: 'Enter ID for department',
+      },
+      {
+        type: 'input',
+        name: 'departmentName',
+        message: 'Enter Name for Department',
+      },
+    ]).then((answers) => {
+      const departmentID = answers.departmentID;
+      const departmentName = answers.departmentName;
+  
+      connection.query('INSERT INTO departments (id, name) VALUES (?, ?)', [departmentID, departmentName], (error, results) => {
+        if (error) throw error;
+        console.log('Department added successfully!');
+        startPrompts();
+      });
+    });
+  }
 
-}
-function addRole() {
-    console.log('5');
 
+  function addRole() {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'roleID',
+        message: 'Enter ID for role',
+      },
+      {
+        type: 'input',
+        name: 'roleTitle',
+        message: 'Enter title for role',
+      },
+      {
+        type: 'input',
+        name: 'roleSalary',
+        message: 'Enter salary for role',
+      },
+      {
+        type: 'input',
+        name: 'departmentID',
+        message: 'Enter department ID for the role',
+      },
+    ]).then((answers) => {
+      const roleID = answers.roleID;
+      const roleTitle = answers.roleTitle;
+      const roleSalary = answers.roleSalary;
+      const departmentID = answers.departmentID;
+  
+      connection.query('INSERT INTO roles (id, title, salary, department_id) VALUES (?, ?, ?, ?)', [roleID, roleTitle, roleSalary, departmentID], (error, results) => {
+        if (error) throw error;
+        console.log('Role added successfully!');
+        startPrompts();
+      });
+    });
 }
-function addEmployee(){
-    console.log('6');
 
-}
+function addEmployee() {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'employeeID',
+        message: 'Enter ID for employee',
+      },
+      {
+        type: 'input',
+        name: 'firstName',
+        message: 'Enter first name',
+      },
+      {
+        type: 'input',
+        name: 'lastName',
+        message: 'Enter last name',
+      },
+      {
+        type: 'input',
+        name: 'roleID',
+        message: 'Enter role ID for the employee',
+      },
+      {
+        type: 'input',
+        name: 'managerID',
+        message: 'Enter manager ID for the employee (if applicable)',
+      },
+    ]).then((answers) => {
+      const employeeID = answers.employeeID;
+      const firstName = answers.firstName;
+      const lastName = answers.lastName;
+      const roleID = answers.roleID;
+      const managerID = answers.managerID;
+  
+      connection.query('INSERT INTO employees (id, first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?, ?)', [employeeID, firstName, lastName, roleID, managerID], (error, results) => {
+        if (error) throw error;
+        console.log('Employee added successfully!');
+        startPrompts();
+      });
+    });
+  }
+  
 function updateRole(){
     console.log('7');
 
--- schema.sql
+++ schema.sql
@@ -1,15 +1,47 @@
 DROP DATABASE IF EXISTS manager_hub;
--- Creates the "books_db" database --
+
 CREATE DATABASE manager_hub;
+
 USE manager_hub;
 
--- Add biography table --
-
-
-CREATE TABLE biography(
-
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100),
-    author VARCHAR(250),
-    PRIMARY KEY(id)
+CREATE TABLE departments(
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(100)
 );
+
+CREATE TABLE roles(
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(100),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE employees(
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
+INSERT INTO departments (id, name) VALUES
+(1, 'Sales'),
+(2, 'Marketing'),
+(3, 'Finance'),
+(4, 'Human Resources');
+
+INSERT INTO roles (id, title, salary, department_id) VALUES
+(1, 'Manager', 5000.00, 1),
+(2, 'Salesperson', 3000.00, 1),
+(3, 'Marketing Specialist', 4000.00, 2),
+(4, 'Accountant', 4500.00, 3),
+(5, 'HR Coordinator', 3500.00, 4);
+
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id) VALUES
+(1, 'John', 'Doe', 1, NULL),
+(2, 'Jane', 'Smith', 2, 1),
+(3, 'Michael', 'Johnson', 3, 1),
+(4, 'Emily', 'Davis', 4, 1),
+(5, 'Robert', 'Wilson', 5, 1);
